bfd0955eb9f37600f37cfbdb4247c8bf4558a30d
henrique <henrique.h@ifsp.bra.edu.br>
2022-08-10 21:44:05 -0300

    criando as lists

--- core/views.py
+++ core/views.py
@@ -3,7 +3,27 @@ from random import randint
 from django.http import HttpResponse
 
 def home(request):
-    num = randint(0, 60)
     # contexto = {'dados': num }
-    html = f'<body> Número gerado = {num} </body>'
+    games = []
+    while len(games) < 11:
+        list = []
+
+        while len(list) < 6:
+            num = randint(0, 60)
+            if num not in list:
+                list.append(num)
+
+        list.sort()
+
+        if list not in games:
+            games.append(list)
+
+    html = f'<body> ' \
+                f'<p>Simulador de Loteria</p>' \
+                    '{% for i in loteria %}' \
+                        'jogo = {{i}}' \
+                    '{% endfor%}'\
+           f'<p></p>' \
+           f'Jogo = {games}</body>'
+
     return HttpResponse(html)
