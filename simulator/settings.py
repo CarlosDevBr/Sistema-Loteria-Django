20ad22c193a9ab54072cb269acc8f8587cd34d4c
henrique <henrique.h@aluno.ifsp.bra.br>
2022-08-17 20:32:49 -0300

    adicionado imagem

--- simulator/settings.py
+++ simulator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media_files/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_files')
+
+print('O base dir é: ', BASE_DIR)
+# MEDIA_URL é uma string usada para montar a url das imagens
+# lado esquerdo concatena o end do server, lado direito o nome do arquivo
+
+# MEDIA_ROOT usado para indicar o caminho dos arquivos no disco
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
